ff29c3f4368d300c0a2358628a51b6f93febbb95
elliot! <benjikrolfanpageuwu@gmail.com>
2025-05-10 09:02:10 +0100

    Update PluginInfo.cs
    changed the name of the mod + added my name

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -1,12 +1,12 @@
-﻿namespace TooMuchInfo
+namespace TooMuchInfo
 {
     /// <summary>
     /// This class is used to provide information about your mod to BepInEx.
     /// </summary>
     internal class PluginInfo
     {
-        public const string GUID = "com.goldentrophy.gorillatag.toomuchinfo";
-        public const string Name = "TooMuchInfo";
+        public const string GUID = "com.elliot.gorillatag.someinfo";
+        public const string Name = "SomeInfo";
         public const string Version = "1.0.0";
     }
 }
